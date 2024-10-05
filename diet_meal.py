b7c05a44d91dfadd8236c2b2199e02818126daf8
pikakn <akanaritamade@outlook.jp>
2024-10-05 22:27:57 +0900

    データベースと沿う形にメニューを変更

--- diet_meal.py
+++ diet_meal.py
@@ -1,9 +1,10 @@
 import streamlit as st
 st.title("食事　摂取カロリー")
 cerbo_options = ["ご飯","パン","うどん"]
-mainmeal_options = ["唐揚げ","とんかつ","ハンバーグ","焼き鳥","生姜焼き"]
-sidemeal_options = ["サラダ","漬物","カプレーゼ"]
+mainmeal_options = ["卵焼き","納豆","焼き魚(鮭)","ハンバーグ"]
+submeal_options = ["サラダ","味噌汁"]
+sidemenu_options = ["牛乳200ml","ヨーグルト1パック","みかん","りんご"]
 
 cerbo = st.selectbox("主食の種類を選んでください:", cerbo_options)
 mainmeal = st.selectbox("主菜の種類を選んでください", mainmeal_options)
-sidemeal = st.selectbox("副菜の種類を選んでください")
+submeal = st.selectbox("副菜の種類を選んでください")
