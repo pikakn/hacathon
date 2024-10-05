45eae552f04035532b1ea13ead80720f39600aeb
pikakn <akanaritamade@outlook.jp>
2024-10-05 17:39:49 +0900

    exerciseGo の実装

--- database_food.py
+++ database_food.py
@@ -1,15 +1,17 @@
 class person:
-    def __init__(self,name,age,sex,height,weight,object_gram):
-        self.name = name
+    def __init__(self,user,age,sex,height,weight,object_gram):
+        self.user = user
         self.age = age
         self.sex = sex
         self.height = height
         self.weight = weight
         self.object_gram = object_gram
-class diet:
-    def __init__(self,name,user,food,exercise,calorie):
+        
+
+class diet():
+    def __init__(self,name,usern,food,exercise,calorie):
         self.name = name
-        self.user = user
+        self.usern = usern
         self.food = food
         self.exercise = exercise
         self.calorie = calorie
@@ -17,12 +19,38 @@ class diet:
     def add_calorie(self,calorie):
         self.calorie += calorie
 
+    def exerciseGo(self,weight,exercise):
+        import time
+        weight = float(weight)
+        exercise = float(exercise)
+        start = time.time()
+        a = input("let's start exercise!")
+        b = input("press enter to end exercise")
+        end = time.time()
+        print(end-start)
+        duration = end-start
+        calorie = exercise*weight*duration*1.05
+        self.calorie -= calorie
     
+
+#消費カロリー(kcal) ＝ メッツ × 体重(kg)×運動時間(分) ×1.05
+#運動の消費カロリー
+cycling = diet("自転車", 0, 0, 1, 4)
+walking = diet("ウォーキング", 0, 0, 1, 3)
+running = diet("ランニング", 0, 0, 1, 7)
+dance = diet("ダンス", 0, 0, 1, 4)
+swimming = diet("水泳", 0, 0, 1, 5)
+weak_strength_training = diet("筋トレ(低強度)", 0, 0, 1, 4)
+strong_strength_training = diet("筋トレ(高強度)", 0, 0, 1, 8)
+yoga = diet("ヨガ", 0, 0, 1, 2.5)
+trekking = diet("登山", 0, 0, 1, 5)
+weak_sports = diet("スポーツ", 0, 0, 1, 4)
+strong_sports = diet("スポーツ(激しめ)", 0, 0, 1, 8)    
     
         
 print("Let's begin with filling your data,name,age,sex,height,weight,and object_gram.")
 you_user = person(input(),input(),input(),input(),input(),input())
-you = diet(you_user.name,1,0,0,0) 
+you = diet(you_user.user,1,0,0,0) 
 print("you begin the diet!")       
 
 
@@ -31,16 +59,7 @@ apple = diet("りんご",1,0,0,135)
 
 diet.add_calorie(you,apple.calorie)
 
-def exerciseGo(you,exercise):
-    import time
-    start = time.time()
-    a = input("let's start exercise!")
-    b = input("press enter to end exercise")
-    end = time.time()
-    print(end-start)
-    duration = end-start
-    exercise.calorie
+diet.exerciseGo(you,you_user.weight,cycling.calorie)
 
 print(you.calorie)
-
 
