9dd8593abf24c9b7cb16b0ab35eb6d9419a7b1e5
pikakn <akanaritamde@outlook.jp>
2024-10-06 13:11:17 +0900

    diet_meal にカロリー計算を実装

--- diet_meal.py
+++ diet_meal.py
@@ -72,38 +72,62 @@ import pandas as pd
 import datetime
 
 st.title("食事　摂取カロリー")
-cerbo_options = ["","ご飯","パン","うどん"]
-mainmeal_options = ["","卵焼き","納豆","焼き魚(鮭)","ハンバーグ"]
-submeal_options = ["","サラダ","味噌汁"]
-sidemenu_options = ["","牛乳200ml","ヨーグルト1パック","みかん","りんご"]
-
-cerbo = st.selectbox("主食の種類を選んでください:", cerbo_options)
-mainmeal = st.selectbox("主菜の種類を選んでください", mainmeal_options)
-submeal = st.selectbox("副菜の種類を選んでください", submeal_options)
-sidemenu = st.selectbox("サイドメニューの種類を選んでください",sidemenu_options)
-
 you_user = person("John",23,"male",175,63,3,[65])
 you = diet(you_user.user,1,0,0,0) 
 
 
+cerbo_options = [nothing,rice,bread,udon]
+mainmeal_options = [nothing,japomlett,natto,friedfish,hamberg]
+submeal_options = [nothing,salad,misosoup]
+sidemenu_options = [nothing,milk,yorgurt,orenge,apple]
+
+cerbo = st.selectbox("主食の種類を選んでください:", cerbo_options,format_func=lambda x:x.name)
+mainmeal = st.selectbox("主菜の種類を選んでください", mainmeal_options,format_func=lambda x:x.name)
+submeal = st.selectbox("副菜の種類を選んでください", submeal_options,format_func=lambda x:x.name)
+sidemenu = st.selectbox("サイドメニューの種類を選んでください",sidemenu_options,format_func=lambda x:x.name)
+
+
+
+
 # 表示
+df2 = pd.DataFrame({
+        "日時":"表示サンプル",
+        "主食":"",
+        "主菜":"",
+        "副菜":"",
+        "サイドメニュー":"",
+        "カロリー":"0"
+        },index=[0])
 # session_state でボタンによる入力を保持 辞書の形式で格納している
 if "eatlist" not in st.session_state:
-    st.session_state.eatlist = []
+    st.session_state.eatlist = [df2]
 st.write("lets eat!")
 
 
 if st.button("食べる"): #食べたもののリストが累積していく　データベースとの照合はあと
     datenow = datetime.datetime.now()
     date = "{}/{}/{}".format(datenow.year,datenow.month,datenow.day)
+    calorie_menu = cerbo.calorie + mainmeal.calorie + submeal.calorie + sidemenu.calorie
+    calorie_menu = str(calorie_menu)
     dfnow = pd.DataFrame({
         "日時":date,
-        "主食":"a",
-        "主菜":"b",
-        "副菜":"c",
-        "カロリー":"d"
+        "主食":cerbo.name,
+        "主菜":mainmeal.name,
+        "副菜":submeal.name,
+        "サイドメニュー":sidemenu.name,
+        "カロリー":calorie_menu
         },index=[0])
     st.session_state.eatlist.append(dfnow)    
-    df2 = pd.concat(st.session_state.eatlist)
-    st.write(df2) 
+df2 = pd.concat(st.session_state.eatlist)
+st.write(df2) 
 
+sum_calorie_menu = 0
+for i in st.session_state.eatlist:
+    sum_calorie_menu += int(i.iat[0,5])
+
+st.write(f"{sum_calorie_menu}カロリーの摂取だ")
+
+diet.add_calorie(you,sum_calorie_menu)
+# データyouに情報を更新
+st.write(f"今のカロリーは{you.calorie}だ！")
+        
